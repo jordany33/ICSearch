d8d64d0acb919b3c89ca58ea67bdc8949a6d6063
Oscar Tran <otran2@circinus-33.ics.uci.edu>
2024-05-06 18:22:37 -0700

    Added global dicts for document id mapped to urls and index

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin, urldefrag
 import hashlib
 
+index = {}
+docMap = {}
 text_documents = set()
 
 def build_index(text_documents):
