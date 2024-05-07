c86374537bd7d7f588f94d4cc2118202d15aca44
Jordan David Yee <jordady1@circinus-34.ics.uci.edu>
2024-05-06 18:36:51 -0700

    imported zipfile

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 import re
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse, urljoin, urldefrag
 import hashlib
 import nltk
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin, urldefrag
+from zipfile import ZipFile 
 
 index = {}
 docMap = {}
 text_documents = set()
+
 
 def build_index(text_documents):
     pass
