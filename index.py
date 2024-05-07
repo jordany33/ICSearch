acb0f40f7f6b711cccd3f30f0e2ac9efbeccf484
Jordan David Yee <jordady1@circinus-39.ics.uci.edu>
2024-05-06 18:04:11 -0700

    Set up structure of index.py

---
+++ index.py
@@ -0,0 +1,9 @@
+import re
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin, urldefrag
+import hashlib
+
+text_documents = set()
+
+def build_index(text_documents):
+    pass
