01654501d379b8b6bc768d9249592d60bd31bef4
Jordan Yee <jrdeyee@gmail.com>
2024-05-30 14:36:32 -0700

    Calculating response time

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from flask import Flask, request, render_template
 from nltk.stem import PorterStemmer
 import pickle
@@ -6,11 +7,12 @@ from index import tokenize
 from search import makeQueryWeights, extractFromIndex, resultsByRelevance
 
 app = Flask(__name__)
-#Decide which index of index and index to use based on if a champion list argument is given
+
+# Decide which index of index and index to use based on if a champion list argument is given
 indexOfIndexName = "indexOfIndexes"
 indexName = "FinalIndex"
-#Construct string based on arguments
-if len(sys.argv)==2:
+# Construct string based on arguments
+if len(sys.argv) == 2:
     indexOfIndexName = indexOfIndexName + sys.argv[1]
     indexName = indexName + sys.argv[1]
 
@@ -25,17 +27,18 @@ def home():
     return render_template('home.html')
 
 @app.route('/results', methods=['POST', 'GET'])
-
 def results():
     query = request.args.get('query')
 
     if request.method == 'POST':
         query = request.form['query'].strip().lower()
-        
+
     page = request.args.get('page', 1, type=int)
     max_per_page = 10
     start = (page - 1) * max_per_page
     end = start + max_per_page
+
+    start_time = time.time()
 
     porter_stemmer = PorterStemmer()
     tokens = [porter_stemmer.stem(x) for x in tokenize(query)]
@@ -51,10 +54,12 @@ def results():
         total_results = 0
         documents = []
 
+    response_time = (time.time() - start_time) * 1000
+
     next_page = page + 1 if end < total_results else None
     prev_page = page - 1 if start > 0 else None
 
-    return render_template('results.html', query=query, documents=documents, next_page=next_page, prev_page=prev_page)
+    return render_template('results.html', query=query, documents=documents, response_time=response_time, next_page=next_page, prev_page=prev_page)
 
 if __name__ == "__main__":
     app.run(debug=True)
